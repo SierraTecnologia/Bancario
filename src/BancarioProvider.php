de571be26656c5f6446be87e62384f4f41c744c6
ricardo@sierratecnologia.com.br <ricardo@sierratecnologia.com.br>
2020-11-18 23:16:19 -0300

    Ajeigtando problemas no porteiri

--- src/BancarioProvider.php
+++ src/BancarioProvider.php
@@ -58,7 +58,7 @@ class BancarioProvider extends ServiceProvider
                 'label_color' => 'success',
                 'section' => "master",
                 'level'       => 3, // 0 (Public), 1, 2 (Admin) , 3 (Root)
-                // 'access' => \App\Models\Role::$ADMIN
+                // 'access' => \Porteiro\Models\Role::$ADMIN
             ],
         ],
     ];
